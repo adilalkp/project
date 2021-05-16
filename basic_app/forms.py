e8995ac42558f62da1dd5aee47a88bd9e7a51109
Sinadin Zidan K <sinadinzidanbava@gmail.com>
2021-05-17 01:13:06 +0530

    Login page setup

--- basic_app/forms.py
+++ basic_app/forms.py
@@ -3,5 +3,5 @@ from django.forms import (CharField, TextInput, PasswordInput)
 
 
 class LoginForm(forms.Form):
-    username = CharField(max_length=30, widget=TextInput(attrs={'class': 'form__input', 'placeholder':'username', 'id':'username'}))
-    password = CharField(widget=PasswordInput(attrs={'class': 'form__input', 'placeholder':'password', 'id':'password'}))
+    username = CharField(max_length=30, widget=TextInput(attrs={'class': 'form__input', 'placeholder':'Username', 'id':'username'}))
+    password = CharField(widget=PasswordInput(attrs={'class': 'form__input', 'placeholder':'Password', 'id':'password'}))
