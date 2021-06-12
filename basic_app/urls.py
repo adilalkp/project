288f0259710a73cdb3d5c8345296df5594902d57
adilalkp <adilarsenic@gmail.com>
2021-06-12 14:19:37 +0530

    export pdf done

--- basic_app/urls.py
+++ basic_app/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('logout', views.logout_view, name="logout"),
     path('list_jobs', views.list_jobs, name="list_jobs"),
     path('job/<uuid:job_code>', views.individual_job, name="ind_job"),
+    path('report', views.report),
+    path('generate/<uuid:job_code>/<slug:key>', views.generate_key, name="generate_key"),
+    path('generate/<uuid:job_code>', views.generate, name="generate"),
 
 ]
